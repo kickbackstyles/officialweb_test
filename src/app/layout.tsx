7f105c092e58ba34a7dfd30d35e71baf716a7508
RaitoTanaka <kickbackstyles.official@gmail.com>
2025-02-28 09:45:17 +0900

    layout.tsx semi&quarto 修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import './globals.css';
 import { Providers } from '../../providers';
@@ -43,4 +43,4 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       </body>
     </html>
   );
-}
+};
